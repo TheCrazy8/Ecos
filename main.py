48ed478054511c18f8b09581ece3e0ccab57096c
Scarlett/Eight <bennettbashford@gmail.com>
2026-01-20 15:09:21 -0500

    haha get dunked on

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import datetime
 import sys
 import os
-import math
-import random
+import webbrowser
 
 now = datetime.datetime.now()
 namespace = os.getenv('USER', 'Guest')
 
 def greet_user(name):
-    return f"Hello, {name}! Today is {now.strftime('%Y-%m-%d')}."
+    return f"Hello, {name}! Today is {now.strftime('%Y-%m-%d')}.\n Welcome to the Hacking Tool!"
 
 def modify_windows():
-    if sys.platform.startswith('win'):
-        try:
-            modify_windows_specific_settings()
-            return "Success"
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return "Error"
-    else:
-        print("This script is intended to run on Windows systems.")
+    try:
+        modify_windows_specific_settings()
+        return "Success"
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return "Error"
 
 def modify_windows_specific_settings():
     # Placeholder for Windows-specific modifications
-    print("Placeholder")
-    return "Placeholder"
+    print("You really thought...\nGet dunked on.")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return "Lol"
 
 def main():
     print(greet_user(namespace))
